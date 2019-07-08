a7c1555377af07f6a90fd3ca704dbc777f7b2463
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-07-08 13:24:28 +0200

    Added optional query params (startdate,enddate,page,pagesize).

--- src/StarWars/StarWarsQuery.cs
+++ src/StarWars/StarWarsQuery.cs
@@ -15,13 +15,19 @@ namespace StarWars
             Field<IssueType>(
                 "issue",
                 arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the issue" }
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "Id of the issue" }
                 ),
                 resolve: context => data.GetIssueByIdAsync(context.GetArgument<string>("id"))
             );
 
             Field<ListGraphType<IssueType>>(
                 "issues",
+                arguments: new QueryArguments(
+                    new QueryArgument<StringGraphType> { Name = "startdate", Description = "Start date to filter by" },
+                    new QueryArgument<StringGraphType> { Name = "enddate", Description = "End date to filter by" },
+                    new QueryArgument<IntGraphType> { Name = "page", Description = "Page of the response" },
+                    new QueryArgument<IntGraphType> { Name = "pagesize", Description = "Page size of the response" }
+                ),
                 resolve: context => data.GetAllIssues()
             );
 
